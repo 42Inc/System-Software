efd803d03a24df5988f53bf1179d0bfca35c5478
Doshinsky <akter1997@gmail.com>
2019-03-04 16:34:58 +0700

    Fixed Makefile and copied some test code from lecture

--- Lab-4/src/malloc_stats.c
+++ Lab-4/src/malloc_stats.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <malloc.h>
 
 int main(int argc, char const *argv[])
 {
-  /* code */
+  char * str;
+  int * array;
+  str = (char *) malloc (15);
+  array = (int *) calloc (sizeof(int), 10);
+  if (str == NULL) {
+    printf ("Error!\n");
+    exit(EXIT_FAILURE);
+  }
+  strcpy (str, "Первый раз");
+  array[4] = strlen(str);
+  printf ("Строка [%s] имеет длинну %d символов\n", str, array[4]);
+  free (str);
+  free (array);
   return EXIT_SUCCESS;
 }
