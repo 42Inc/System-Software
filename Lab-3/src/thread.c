56bfe01d7160d588854e4ef73819ba2acfe2fa94
Doshinsky <akter1997@gmail.com>
2019-03-05 15:22:11 +0700

    Finished 3rd lab, also reconfigured Makefile for it. Now we need to pass it:D

--- Lab-3/src/thread.c
+++ Lab-3/src/thread.c
@@ -1,27 +1,30 @@
-#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 int i = 0;
 
-void* increment() {
+void* increment()
+{
   int j;
+  for (j = 0; j < 100000; ++j) ++i;
 
-  for (j = 0; j < 100000; ++j)
-    ++i;
-
+  return NULL;
 }
 
-int main() {
+int main(int argc, char const *argv[])
+{
   pthread_t tid = -1;
   pthread_attr_t attr;
   int j;
+
   pthread_attr_init(&attr);
   pthread_create(&tid, &attr, increment, NULL);
 
-  for (j = 0; j < 100000; ++j)
-    ++i;
+  for (j = 0; j < 100000; ++j) ++i;
 
-  if (tid != -1)
-    pthread_join(tid, NULL);
+  if (tid != -1) pthread_join(tid, NULL);
   printf("i = %d (expected: 200000)\n", i);
+
+  return EXIT_SUCCESS;
 }
